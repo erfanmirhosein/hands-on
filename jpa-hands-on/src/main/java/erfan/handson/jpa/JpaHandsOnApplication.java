4bb04c5b2d1b252c4e3be9b18747148a86f9ec62
Erfan <erfanscott2@gmail.com>
2025-03-19 11:48:59 +0330

    create an initial project setup

---
+++ jpa-hands-on/src/main/java/erfan/handson/jpa/JpaHandsOnApplication.java
@@ -0,0 +1,12 @@
+package erfan.handson.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHandsOnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaHandsOnApplication.class, args);
+    }
+}
