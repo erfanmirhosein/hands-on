a95d5643eb23f7825ee639a8c7d233025e39340e
Erfan <erfanscott2@gmail.com>
2025-09-20 18:42:10 +0330

    use parameterized test for sorting algorithms

--- dsa/src/test/java/erfan/handson/dsa/algorithm/sorting/SortingAlgorithmTest.java
+++ dsa/src/test/java/erfan/handson/dsa/algorithm/sorting/SortingAlgorithmTest.java
@@ -1,33 +1,42 @@
 package erfan.handson.dsa.algorithm.sorting;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SortingAlgorithmTest {
-    @Test
-    void givenAnArray_whenIUseSortTheArray_thenTheResultShouldBeCorrect() {
-        int[] unsortedArray = new int[]{5, 6, 5, 1, -2, 3};
-        int[] sortedArray = new int[]{-4, -3, -2, -1, 0, 1};
-        int[] emptyArray = new int[]{};
-        int[] oneElementArray = new int[]{-8};
+    @ParameterizedTest
+    @ArgumentsSource(SortingTestArgumentProvider.class)
+    void givenAnArray_whenIUseSortTheArray_thenTheResultShouldBeCorrect(int[] unsortedArray, int[] expectedResult) {
         SortingAlgorithm bubbleSortAlgorithm = new BubbleSort();
         SortingAlgorithm selectionSortAlgorithm = new SelectionSort();
         SortingAlgorithm insertionSortAlgorithm = new InsertionSort();
 
-        assertThat(bubbleSortAlgorithm.sort(unsortedArray)).isEqualTo(new int[]{-2, 1, 3, 5, 5, 6});
-        assertThat(bubbleSortAlgorithm.sort(emptyArray)).isEqualTo(new int[]{});
-        assertThat(bubbleSortAlgorithm.sort(oneElementArray)).isEqualTo(new int[]{-8});
-        assertThat(bubbleSortAlgorithm.sort(sortedArray)).isEqualTo(new int[]{-4, -3, -2, -1, 0, 1});
+        assertThat(bubbleSortAlgorithm.sort(unsortedArray)).isEqualTo(expectedResult);
+        assertThat(selectionSortAlgorithm.sort(unsortedArray)).isEqualTo(expectedResult);
+        assertThat(insertionSortAlgorithm.sort(unsortedArray)).isEqualTo(expectedResult);
+    }
 
-        assertThat(selectionSortAlgorithm.sort(unsortedArray)).isEqualTo(new int[]{-2, 1, 3, 5, 5, 6});
-        assertThat(selectionSortAlgorithm.sort(emptyArray)).isEqualTo(new int[]{});
-        assertThat(selectionSortAlgorithm.sort(oneElementArray)).isEqualTo(new int[]{-8});
-        assertThat(selectionSortAlgorithm.sort(sortedArray)).isEqualTo(new int[]{-4, -3, -2, -1, 0, 1});
-
-        assertThat(insertionSortAlgorithm.sort(unsortedArray)).isEqualTo(new int[]{-2, 1, 3, 5, 5, 6});
-        assertThat(insertionSortAlgorithm.sort(emptyArray)).isEqualTo(new int[]{});
-        assertThat(insertionSortAlgorithm.sort(oneElementArray)).isEqualTo(new int[]{-8});
-        assertThat(insertionSortAlgorithm.sort(sortedArray)).isEqualTo(new int[]{-4, -3, -2, -1, 0, 1});
+    static class SortingTestArgumentProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                    Arguments.of(
+                            new int[]{5, 6, 5, 1, -2, 3}, new int[]{-2, 1, 3, 5, 5, 6}
+                    ), Arguments.of(
+                            new int[]{}, new int[]{}
+                    ), Arguments.of(
+                            new int[]{-8}, new int[]{-8}
+                    ), Arguments.of(
+                            new int[]{-4, -3, -2, -1, 0, 1}, new int[]{-4, -3, -2, -1, 0, 1}
+                    )
+            );
+        }
     }
 }
